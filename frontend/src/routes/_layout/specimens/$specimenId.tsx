fb394a8e6fbd68978be51d40593b388b833da3f8
Daniel R <daniel.razavi@utoronto.ca>
2026-01-16 17:50:54 -0500

    Refactor SpecimenDetails component: implement tabbed navigation for improved data organization and presentation

--- frontend/src/routes/_layout/specimens/$specimenId.tsx
+++ frontend/src/routes/_layout/specimens/$specimenId.tsx
@@ -13,6 +13,12 @@ import {
   ItemDescription,
   ItemTitle
 } from "@/components/ui/item"
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/components/ui/tabs"
 import { ExternalLinkIcon } from "lucide-react"
 import { Separator } from "@/components/ui/separator";
 import { createFileRoute, Link } from "@tanstack/react-router";
@@ -81,112 +87,125 @@ function SpecimenDetails() {
 				</Button>
 			</div>
 			<div className="grid gap-6 text-sm">
-				<Card>
-					<CardHeader>
-						<CardTitle className="text-2xl">Meta Data</CardTitle>
-					</CardHeader>
-					<CardContent className="grid gap-4 md:grid-cols-[2fr_auto_1fr] md:items-start">
-						<div className="grid gap-3">
-							<h3 className="text-xl font-semibold tracking-tight text-foreground">Specimen Information</h3>
-							<div className="grid gap-4 md:grid-cols-2">
+				<Tabs defaultValue="Meta Data">
+					<TabsList>
+						<TabsTrigger value="Meta Data">Meta Data</TabsTrigger>
+						<TabsTrigger value="Structural Data">Structural Data</TabsTrigger>
+						<TabsTrigger value="Experimental Data">Experimental Data</TabsTrigger>
+					</TabsList>
+					<TabsContent value="Meta Data">
+						<Card>
+							<CardHeader>
+								<CardTitle className="text-2xl">Meta Data</CardTitle>
+							</CardHeader>
+							<CardContent className="grid gap-4 md:grid-cols-[2fr_auto_1fr] md:items-start">
+								<div className="grid gap-3">
+									<h3 className="text-xl font-semibold tracking-tight text-foreground">Specimen Information</h3>
+									<div className="grid gap-4 md:grid-cols-2">
+										<div className="grid gap-2">
+											<LabelValue label="Assembly Type" value={renderValue(data.assembly_type)} />
+											<LabelValue label="Joinery Type" value={renderValue(data.joinery_type.label)} />
+											<LabelValue label="Sub Joinery Type" value={renderValue(data.sub_joinery_type.label)} />
+											<LabelValue label="Fastener Types" value={renderLabels(data.fastener_types)} />
+											<LabelValue label="Loading Directions" value={renderLabels(data.loading_directions)} />
+											<LabelValue label="Practice" value={renderValue(data.practice)} />
+										</div>
+										<div className="grid gap-2">
+											<LabelValue label="Fastener Numbers" value={renderValue(data.fastener_numbers)} />
+											<LabelValue label="Connector Present" value={renderValue(data.connector)} />
+											<LabelValue label="Dowel Present" value={renderValue(data.dowel)} />
+											<LabelValue label="Replicate Tests" value={renderValue(data.replicate_tests)} />
+											<LabelValue label="Connection Description" value={renderValue(data.connection_description)} />
+											<LabelValue label="Note" value={renderValue(data.note)} />
+										</div>
+									</div>
+								</div>
+								<Separator orientation="vertical" className="hidden md:block"/>
 								<div className="grid gap-2">
-									<LabelValue label="Assembly Type" value={renderValue(data.assembly_type)} />
-									<LabelValue label="Joinery Type" value={renderValue(data.joinery_type.label)} />
-									<LabelValue label="Sub Joinery Type" value={renderValue(data.sub_joinery_type.label)} />
-									<LabelValue label="Fastener Types" value={renderLabels(data.fastener_types)} />
-									<LabelValue label="Loading Directions" value={renderLabels(data.loading_directions)} />
-									<LabelValue label="Practice" value={renderValue(data.practice)} />
-								</div>
-								<div className="grid gap-2">
-									<LabelValue label="Fastener Numbers" value={renderValue(data.fastener_numbers)} />
-									<LabelValue label="Connector Present" value={renderValue(data.connector)} />
-									<LabelValue label="Dowel Present" value={renderValue(data.dowel)} />
-									<LabelValue label="Replicate Tests" value={renderValue(data.replicate_tests)} />
-									<LabelValue label="Connection Description" value={renderValue(data.connection_description)} />
-									<LabelValue label="Note" value={renderValue(data.note)} />
-								</div>
-							</div>
-						</div>
-						<Separator orientation="vertical" className="hidden md:block"/>
-						<div className="grid gap-2">
-							<h3 className="text-xl font-semibold tracking-tight text-foreground">Identification Information</h3>
-							<Item variant="outline" asChild>
-								<div>
-									<ItemContent>
-										<ItemTitle>Specimen ID</ItemTitle>
-										<ItemDescription>{data.id}</ItemDescription>
-									</ItemContent>
-								</div>
-							</Item>
+									<h3 className="text-xl font-semibold tracking-tight text-foreground">Identification Information</h3>
+									<Item variant="outline" asChild>
+										<div>
+											<ItemContent>
+												<ItemTitle>Specimen ID</ItemTitle>
+												<ItemDescription>{data.id}</ItemDescription>
+											</ItemContent>
+										</div>
+									</Item>
 
-							<Item variant="outline" asChild>
-								<Link
-									to="/doi/$doiId"
-									params={{ doiId: data.doi.id }}
-									target="_blank"
-									className="block"
-								>
-									<ItemContent>
-										<ItemTitle>View DOI</ItemTitle>
-										<ItemDescription>
-											{data.doi.ref_title}
-										</ItemDescription>
-									</ItemContent>
-									<ItemActions>
-										<ExternalLinkIcon className="size-4" />
-									</ItemActions>
-								</Link>
-							</Item>
-						</div>
-					</CardContent>
-				</Card>
-				<Card>
-					<CardHeader>
-						<CardTitle className="text-2xl">Structural Data</CardTitle>
-					</CardHeader>
-					<CardContent className="grid gap-4 md:grid-cols-[1fr_auto_2fr] md:items-start">
-						<div className="grid gap-2">
-							<h3 className="text-xl font-semibold tracking-tight text-foreground">Geometric Properties</h3>
-							<LabelValue label="Element Dimension" value={renderValue(data.element_dimension)} />
-							<MoistureDial value={moistureChartData[0].moisture} label="Moisture Percentage" className="max-w-[200px]" />
-						</div>
-						<Separator orientation="vertical" className="hidden md:block"/>
-						<div className="grid gap-2">
-							<h3 className="text-xl font-semibold tracking-tight text-foreground">Material Properties</h3>
-							<LabelValue label="Wood Type" value={renderValue(data.wood_type)} />
-							<LabelValue label="Wood Mechanical Properties" value={renderValue(data.wood_mechanical_properties)} />
-							<LabelValue label="Fastener Mechanical Properties" value={renderValue(data.fastener_mechanical_properties)} />
-							<LabelValue label="Connector Mechanical Properties" value={renderValue(data.connector_mechanical_properties)} />
-						</div>
-					</CardContent>
-				</Card>
-				<Card>
-					<CardHeader>
-						<CardTitle className="text-2xl">Experimental Data</CardTitle>
-					</CardHeader>
-					<CardContent className="grid gap-6 md:grid-cols-2 md:items-start">
-						<div className="grid gap-2">
-							<h3 className="text-xl font-semibold tracking-tight text-foreground">Experimental Results</h3>
-							<LabelValue label="Experiment Date" value={renderValue(data.e_date)} />
-							<LabelValue label="Test Loading Type" value={renderValue(data.e_test_loading_type)} />
-							<LabelValue label="Measurement Unit" value={renderValue(data.e_measurement_unit)} />
-							<LabelValue label="Yield Point Method" value={renderValue(data.e_yield_point_method)} />
-							<LabelValue label="Note" value={renderValue(data.note)} />
-							
-							<div className="grid gap-2">
-								<h3 className="text-xl font-semibold tracking-tight text-foreground">Qualitative Failure Measures</h3>
-								<LabelValue label="Failure Modes" value={renderLabels(data.e_qualitative_failure_measure)} />
-								<LabelValue label="QFM Description" value={renderValue(data.e_qfm_description)} />
-							</div>
-						</div>
-						<div className="grid gap-6">
-							<div className="grid gap-2">
-								<h3 className="text-xl font-semibold tracking-tight text-foreground">Quantitative Mechanical Measures</h3>
-								<RadarMetricsChart data={data} />
-							</div>
-						</div>
-					</CardContent>
-				</Card>
+									<Item variant="outline" asChild>
+										<Link
+											to="/doi/$doiId"
+											params={{ doiId: data.doi.id }}
+											target="_blank"
+											className="block"
+										>
+											<ItemContent>
+												<ItemTitle>View DOI</ItemTitle>
+												<ItemDescription>
+													{data.doi.ref_title}
+												</ItemDescription>
+											</ItemContent>
+											<ItemActions>
+												<ExternalLinkIcon className="size-4" />
+											</ItemActions>
+										</Link>
+									</Item>
+								</div>
+							</CardContent>
+						</Card>
+					</TabsContent>
+					<TabsContent value="Structural Data">
+						<Card>
+							<CardHeader>
+								<CardTitle className="text-2xl">Structural Data</CardTitle>
+							</CardHeader>
+							<CardContent className="grid gap-4 md:grid-cols-[1fr_auto_2fr] md:items-start">
+								<div className="grid gap-2">
+									<h3 className="text-xl font-semibold tracking-tight text-foreground">Geometric Properties</h3>
+									<LabelValue label="Element Dimension" value={renderValue(data.element_dimension)} />
+									<MoistureDial value={moistureChartData[0].moisture} label="Moisture Percentage" className="max-w-[200px]" />
+								</div>
+								<Separator orientation="vertical" className="hidden md:block"/>
+								<div className="grid gap-2">
+									<h3 className="text-xl font-semibold tracking-tight text-foreground">Material Properties</h3>
+									<LabelValue label="Wood Type" value={renderValue(data.wood_type)} />
+									<LabelValue label="Wood Mechanical Properties" value={renderValue(data.wood_mechanical_properties)} />
+									<LabelValue label="Fastener Mechanical Properties" value={renderValue(data.fastener_mechanical_properties)} />
+									<LabelValue label="Connector Mechanical Properties" value={renderValue(data.connector_mechanical_properties)} />
+								</div>
+							</CardContent>
+						</Card>
+					</TabsContent>
+					<TabsContent value="Experimental Data">
+						<Card>
+							<CardHeader>
+								<CardTitle className="text-2xl">Experimental Data</CardTitle>
+							</CardHeader>
+							<CardContent className="grid gap-6 md:grid-cols-2 md:items-start">
+								<div className="grid gap-2">
+									<h3 className="text-xl font-semibold tracking-tight text-foreground">Experimental Results</h3>
+									<LabelValue label="Experiment Date" value={renderValue(data.e_date)} />
+									<LabelValue label="Test Loading Type" value={renderValue(data.e_test_loading_type)} />
+									<LabelValue label="Measurement Unit" value={renderValue(data.e_measurement_unit)} />
+									<LabelValue label="Yield Point Method" value={renderValue(data.e_yield_point_method)} />
+									<LabelValue label="Note" value={renderValue(data.note)} />
+									
+									<div className="grid gap-2">
+										<h3 className="text-xl font-semibold tracking-tight text-foreground">Qualitative Failure Measures</h3>
+										<LabelValue label="Failure Modes" value={renderLabels(data.e_qualitative_failure_measure)} />
+										<LabelValue label="QFM Description" value={renderValue(data.e_qfm_description)} />
+									</div>
+								</div>
+								<div className="grid gap-6">
+									<div className="grid gap-2">
+										<h3 className="text-xl font-semibold tracking-tight text-foreground">Quantitative Mechanical Measures</h3>
+										<RadarMetricsChart data={data} />
+									</div>
+								</div>
+							</CardContent>
+						</Card>
+					</TabsContent>
+				</Tabs>
 			</div>
 			{/* <Separator className="my-10" />
 			<pre className="rounded-md bg-muted p-4 text-xs overflow-x-auto whitespace-pre-wrap break-words max-w-full">
