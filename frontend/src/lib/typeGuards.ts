425c2069d49a8171e58baaa516426398b8fe396a
Wisam Al Abed <wisam.alabed@hey.com>
2026-04-09 17:27:55 -0400

    fix: bug with chart not showing

--- frontend/src/lib/typeGuards.ts
+++ frontend/src/lib/typeGuards.ts
@@ -1,17 +1,17 @@
 export function isNumericValue(value: unknown): value is number {
-	return (
-		typeof value === "number" && Number.isNaN(value) && Number.isFinite(value)
-	);
+  return (
+    typeof value === "number" && !Number.isNaN(value) && Number.isFinite(value)
+  );
 }
 
 export function hasNumericProperty<K extends string>(
-	obj: unknown,
-	key: K,
+  obj: unknown,
+  key: K,
 ): obj is Record<K, number> {
-	return (
-		typeof obj === "object" &&
-		obj !== null &&
-		key in obj &&
-		isNumericValue((obj as Record<K, unknown>)[key])
-	);
+  return (
+    typeof obj === "object" &&
+    obj !== null &&
+    key in obj &&
+    isNumericValue((obj as Record<K, unknown>)[key])
+  );
 }
