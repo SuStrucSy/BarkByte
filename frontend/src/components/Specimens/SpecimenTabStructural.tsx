4c95bb59cb9b43ebc5955b873a20032f508327dc
Wisam Al Abed <wisam.alabed@hey.com>
2026-04-16 16:28:03 -0400

    fix: add generated updated api types to frontend

--- frontend/src/components/Specimens/SpecimenTabStructural.tsx
+++ frontend/src/components/Specimens/SpecimenTabStructural.tsx
@@ -7,45 +7,40 @@ import { Separator } from "../ui/separator";
 import { TabsContent } from "../ui/tabs";
 
 type SpecimenTabStructuralProps = {
-	data: SpecimenPublic;
+  data: SpecimenPublic;
 };
 
 export function SpecimenTabStructural({ data }: SpecimenTabStructuralProps) {
-	const moistureValue = parseMoisturePercentage(data.moisture_percentage);
+  const moistureValue = parseMoisturePercentage(data.moisture_percentage);
 
-	return (
-		<TabsContent value="Structural Data">
-			<Card>
-				<CardHeader>
-					<CardTitle className="text-2xl">Structural Data</CardTitle>
-				</CardHeader>
-				<CardContent className="grid gap-4 md:grid-cols-[1fr_auto_2fr] md:items-start">
-					<div className="grid gap-2">
-						<h3 className="text-xl font-semibold tracking-tight text-foreground">
-							Geometric Properties
-						</h3>
-						<LabelValue property="element_dimension" data={data} />
-						<MoistureDial
-							value={moistureValue ?? 0}
-							label="Moisture Percentage"
-							className="m-4"
-						/>
-					</div>
-					<Separator orientation="vertical" className="hidden md:block" />
-					<div className="grid gap-2">
-						<h3 className="text-xl font-semibold tracking-tight text-foreground">
-							Material Properties
-						</h3>
-						<LabelValue property="wood_type" data={data} />
-						<LabelValue property="wood_mechanical_properties" data={data} />
-						<LabelValue property="fastener_mechanical_properties" data={data} />
-						<LabelValue
-							property="connector_mechanical_properties"
-							data={data}
-						/>
-					</div>
-				</CardContent>
-			</Card>
-		</TabsContent>
-	);
+  return (
+    <TabsContent value="Structural Data">
+      <Card>
+        <CardHeader>
+          <CardTitle className="text-2xl">Structural Data</CardTitle>
+        </CardHeader>
+        <CardContent className="grid gap-4 md:grid-cols-[1fr_auto_2fr] md:items-start">
+          <div className="grid gap-2">
+            <h3 className="text-xl font-semibold tracking-tight text-foreground">
+              Geometric Properties
+            </h3>
+            <LabelValue property="element_dimension" data={data} />
+          </div>
+          <Separator orientation="vertical" className="hidden md:block" />
+          <div className="grid gap-2">
+            <h3 className="text-xl font-semibold tracking-tight text-foreground">
+              Material Properties
+            </h3>
+            <LabelValue property="wood_type" data={data} />
+            <LabelValue property="wood_mechanical_properties" data={data} />
+            <LabelValue property="fastener_mechanical_properties" data={data} />
+            <LabelValue
+              property="connector_mechanical_properties"
+              data={data}
+            />
+          </div>
+        </CardContent>
+      </Card>
+    </TabsContent>
+  );
 }
