3ac7f64616017bcb9f615dc30439210c61bb1018
Daniel R <daniel.razavi@utoronto.ca>
2026-04-22 11:28:27 -0400

    fix: scrolling in the specimen page.

--- frontend/src/routes/_layout.tsx
+++ frontend/src/routes/_layout.tsx
@@ -31,7 +31,7 @@ function Layout() {
           isVisible={disclaimerVisible}
           setVisible={setDisclaimerVisible}
         />
-        <div className="flex min-h-0 flex-1 flex-col overflow-hidden p-5">
+        <div className="flex min-h-0 flex-1 flex-col overflow-x-hidden overflow-y-auto p-5">
           <ForbiddenBoundary>
             <Outlet />
           </ForbiddenBoundary>
